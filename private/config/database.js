4a7d16b8ddfd5fdbe4482d4c5f4c4ed159ed08f4
Matheus-Ruivo <matheus.ruivo0522@gmail.com>
2025-11-13 17:19:46 -0300

    feat: add console logs for database connection environment variables

--- private/config/database.js
+++ private/config/database.js
@@ -3,6 +3,11 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_DATABASE);
+
 //conectar com mysql
 const db = await mysql.createConnection({ 
   host: process.env.DB_HOST || "localhost",
