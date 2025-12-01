b77230a123e94d80970c4f9524be8bc03c557cea
SGZ777 <arthursgz77@gmail.com>
2025-12-01 14:52:13 -0300

    Enhance database configuration and clean up HTML files for improved functionality and readability

--- private/config/database.js
+++ private/config/database.js
@@ -7,13 +7,15 @@ console.log(process.env.DB_HOST);
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASSWORD);
 console.log(process.env.DB_DATABASE);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 //conectar com mysql
 const db = await mysql.createConnection({ 
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "dbmt"
+  password: process.env.DB_PASSWORD || "Senai@123",
+  database: process.env.DB_DATABASE || "dbmt",
+  port: process.env.DB_PORT ?? 3308
 });
 
 // exportar o banco
