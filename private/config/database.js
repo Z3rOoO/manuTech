9ad2ddf83dc8fc97a5363a63eb4b8c242ad1cf3a
SGZ777 <arthursgz77@gmail.com>
2025-12-02 13:45:07 -0300

    UPDATE estrutra database.js

--- private/config/database.js
+++ private/config/database.js
@@ -10,12 +10,12 @@ console.log(process.env.DB_DATABASE);
 console.log("DB_PORT:", process.env.DB_PORT);
 
 //conectar com mysql
-const db = await mysql.createConnection({ 
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "dbmt"
-
+const db = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // exportar o banco
