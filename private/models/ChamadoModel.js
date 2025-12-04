3a219a473b0f52ac126005022c8544a33e0d2107
Zer0o0 <henrique.cosmemk4@gmail.com>
2025-12-04 16:33:05 -0300

    UPDATE carrinho

--- private/models/ChamadoModel.js
+++ private/models/ChamadoModel.js
@@ -2,14 +2,14 @@ import db from '../config/database.js';
 
 class ChamadoModel {
     static async criar(dados) {
-        const sql = 'insert into chamado set ?';
+        const sql = 'insert into carrinho set ?';
         const [resultado] = await db.query(sql, [dados]);
         return resultado.insertId;
     }
 
    
     static async listarPorCliente(clienteId) { //busca cliente
-        const sql = 'selecT * from chamado where cliente_id = ?';
+        const sql = 'selecT * from carrinho where cliente_id = ?';
         const [linhas] = await db.query(sql, [clienteId]);
         return linhas;
     }
